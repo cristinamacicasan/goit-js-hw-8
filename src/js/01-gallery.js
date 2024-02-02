ce8b2d4b42a14f765e65454ca69007e21b4336c8
cristinamacicasan <cmacicasan@gmail.com>
2024-02-02 09:38:37 +0200

    add

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,48 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+
+
+
+import { galleryItems } from './gallery-items';
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const galleryList = document.querySelector('.gallery');
+function galleryElementCreation(item) {
+  const galleryItem = document.createElement('li');
+  const galleryLink = document.createElement('a');
+  const galleryImage = document.createElement('img');
+  galleryItem.classList.add('gallery__item');
+  galleryLink.classList.add('gallery__link');
+  galleryImage.classList.add('gallery__image');
+  galleryLink.href = item.original;
+  galleryImage.src = item.preview;
+  galleryImage.alt = item.description;
+  galleryImage.dataset.source = item.original; 
+  galleryItem.appendChild(galleryLink);
+  galleryLink.appendChild(galleryImage);
+  return galleryItem;
+}
+
+
+
+galleryItems.forEach(item => {
+  const galleryItem = galleryElementCreation(item);
+  galleryList.appendChild(galleryItem);
+});
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+gallery.addEventListener("click", (clickEvent) => {
+  clickEvent.preventDefault();
+  lightbox.open();
+});
+
+document.addEventListener('keydown', (escEvent) => {
+  if (escEvent.key === 'Escape' && lightbox.isOpen) {
+    lightbox.close();
+  }
+});
